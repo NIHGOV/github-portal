d7994c9b1b48d1cd1cfb2f8294ac784ed65dc7e0
Alex Williamson <9951123+DevOptimusPrime@users.noreply.github.com>
2022-10-07 16:11:12 -0400

    Update Create-EnvOrgs.ps1
    fix var name

--- .github/scripts/Create-EnvOrgs.ps1
+++ .github/scripts/Create-EnvOrgs.ps1
@@ -1,7 +1,7 @@
-$ouput = Get-Content ./raw.json | ConvertFrom-Json
+$output = Get-Content ./raw.json | ConvertFrom-Json
 
 Write-Output $output.data.enterprise.organizations.nodes
-Write-Host $output.data.enterprise.organizations.nodes
+
 
 $orgsArry = @()
 
