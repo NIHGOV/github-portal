f4aac6e4e371be217be4fa94868ed8794b70495c
Alex Williamson <9951123+DevOptimusPrime@users.noreply.github.com>
2022-10-26 08:43:14 -0400

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+{
+  "apps" : [{
+    "name"        : "portal",
+    "script"      : "./opensource-management-portal/dist/bin/www.js",
+  },{
+    "name"       : "cache-builder",
+    "script"     : "./opensource-management-portal/dist/jobs/refreshQueryCache/index.js",
+    "autorestart": "false"
+  }]
+}
