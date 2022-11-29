2787cba0c67814db5e4fbc2696084b63bb55ed86
Alex Williamson <9951123+DevOptimusPrime@users.noreply.github.com>
2022-11-29 10:07:01 -0500

    Update Create-EnvOrgs.ps1

--- .github/scripts/Create-EnvOrgs.ps1
+++ .github/scripts/Create-EnvOrgs.ps1
@@ -9,7 +9,7 @@ foreach ($org in $output.data.enterprise.organizations.nodes) {
     $envObj = [PSCustomObject]@{
             name = "$($org.login)"
             id = $($org.databaseId)
-            type = @("public", "private")
+            type = @("public", "private", "internal)
             description = "$($org.description)"
             locked = $True
         }
